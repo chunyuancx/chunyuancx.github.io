cd38bdaf558d34a5d8a742da5d3446b48c0dd79a
chunyuancx <64946231+chunyuancx@users.noreply.github.com>
2023-03-06 21:23:56 +0800

    added parallax to introduction

---
+++ parallaxImage.js
@@ -0,0 +1,29 @@
+var rect = $('#introduction')[0].getBoundingClientRect();
+var mouse = { x: 0, y: 0, moved: false };
+
+$('body').mousemove(function (e) {
+  mouse.moved = true;
+  mouse.x = e.clientX - rect.left;
+  mouse.y = e.clientY - rect.top;
+});
+
+// Ticker event will be called on every frame
+TweenLite.ticker.addEventListener('tick', function () {
+  if (mouse.moved) {
+    parallaxIt('#pfp-cropped-image', -13); //strenght of movement
+    parallaxIt('#pfp-cropped-shadow', -20); //strenght of movement
+    parallaxIt('#introduction-banner', -10); //strenght of movement
+  }
+  mouse.moved = false;
+});
+
+function parallaxIt(target, movement) {
+  TweenMax.to(target, 0.5, {
+    x: ((mouse.x - rect.width / 2) / rect.width) * movement,
+    y: ((mouse.y - rect.height / 2) / rect.height) * movement,
+  });
+}
+
+$(window).on('resize scroll', function () {
+  rect = $('#introduction')[0].getBoundingClientRect();
+});
