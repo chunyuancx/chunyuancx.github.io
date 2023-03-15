0a9c799c7e705478f5a916c29f40003013b3fabf
chunyuancx <64946231+chunyuancx@users.noreply.github.com>
2023-03-15 17:06:27 +0800

    afterlife section

--- Javascripts/trailerModal.js
+++ Javascripts/trailerModal.js
@@ -1,25 +1,38 @@
-var lunarnautsModal = document.getElementById('lunarnauts-trailer-Modal');
-var lunarnautsWatchTrailer = document.getElementById('lunarnauts-cta');
-var closeButton = document.getElementsByClassName('trailer-modal-close')[0];
+var closeButton = document.getElementsByClassName('trailer-modal-close');
+console.log(closeButton);
 var iframe = document.querySelectorAll('iframe');
+
+// lunarnauts
+var lunarnautsModal = document.getElementById('lunarnauts-trailer-modal');
+var lunarnautsWatchTrailer = document.getElementById('lunarnauts-cta');
 
 lunarnautsWatchTrailer.onclick = function () {
   lunarnautsModal.style.display = 'block';
 };
 
-closeButton.onclick = function () {
-  lunarnautsModal.style.display = 'none';
-  $('iframe').each(function () {
-    this.contentWindow.postMessage(
-      '{"event":"command","func":"stopVideo","args":""}',
-      '*'
-    );
-  });
+// afterlife
+var afterlifeModal = document.getElementById('afterlife-trailer-modal');
+var afterlifeWatchTrailer = document.getElementById('afterlife-cta');
+
+afterlifeWatchTrailer.onclick = function () {
+  afterlifeModal.style.display = 'block';
 };
 
+// // global
+// afterlifeModal.style.display = 'none';
+// lunarnautsModal.style.display = 'none';
+// $('iframe').each(function () {
+//   this.contentWindow.postMessage(
+//     '{"event":"command","func":"stopVideo","args":""}',
+//     '*'
+//   );
+// });
+
 window.onclick = function (event) {
-  if (event.target == lunarnautsModal) {
+  if (event.target == afterlifeModal || event.target == lunarnautsModal) {
+    afterlifeModal.style.display = 'none';
     lunarnautsModal.style.display = 'none';
+
     $('iframe').each(function () {
       this.contentWindow.postMessage(
         '{"event":"command","func":"stopVideo","args":""}',
