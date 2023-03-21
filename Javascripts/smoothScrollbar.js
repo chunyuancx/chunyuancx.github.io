6044fe86f8072ed0e2728fac644cc64196e95651
chunyuancx <64946231+chunyuancx@users.noreply.github.com>
2023-03-21 23:22:35 +0800

    remove smoothscrollbar
    - it breaks too many things

--- Javascripts/smoothScrollbar.js
+++ Javascripts/smoothScrollbar.js
@@ -1,8 +1,34 @@
-import Scrollbar from './smooth-scrollbar';
+const body = document.body;
+const main = document.getElementById('main');
 
-var options = {
-    'damping': 0.08,
-    'alwaysShowTracks': true
+let sx = 0, // For scroll positions
+  sy = 0;
+let dx = sx, // For container positions
+  dy = sy;
+
+// Bind a scroll function
+window.addEventListener('scroll', easeScroll);
+
+function easeScroll() {
+  sx = window.pageXOffset;
+  sy = window.pageYOffset;
 }
 
-Scrollbar.init(document.querySelector('#my-scrollbar'), options);
+window.requestAnimationFrame(render);
+
+function render() {
+  //We calculate our container position by linear interpolation method
+  dx = li(dx, sx, 0.1);
+  dy = li(dy, sy, 0.1);
+
+  dx = Math.floor(dx * 100) / 100;
+  dy = Math.floor(dy * 100) / 100;
+
+  main.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`;
+
+  window.requestAnimationFrame(render);
+}
+
+function li(a, b, n) {
+  return (1 - n) * a + n * b;
+}
