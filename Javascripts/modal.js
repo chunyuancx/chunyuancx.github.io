27186035ffe59f6469ac7bdc8c27cba817c32e21
chunyuancx <64946231+chunyuancx@users.noreply.github.com>
2023-04-08 02:44:17 +0800

    fixed modal
    cant have 2 event window trackers

--- Javascripts/modal.js
+++ Javascripts/modal.js
@@ -84,18 +84,54 @@ growIncCloseButton.onclick = function () {
   });
 };
 
+//Narratives
+
+//Kindred
+var KindredModal = document.getElementById('kindred-modal');
+var KindredButton = document.getElementById('kindred-explore');
+var KindredCloseButton = document.getElementsByClassName(
+  'explore-modal-close'
+)[0];
+
+KindredButton.onclick = function () {
+  KindredModal.style.display = 'block';
+};
+
+KindredCloseButton.onclick = function () {
+  KindredModal.style.display = 'none';
+};
+
+//Jotun
+var JotunModal = document.getElementById('jotun-modal');
+var JotunButton = document.getElementById('jotun-explore');
+var JotunCloseButton = document.getElementsByClassName(
+  'explore-modal-close'
+)[1];
+
+JotunButton.onclick = function () {
+  JotunModal.style.display = 'block';
+};
+
+JotunCloseButton.onclick = function () {
+  JotunModal.style.display = 'none';
+};
+
 // global
 window.onclick = function (event) {
   if (
     event.target == afterlifeModal ||
     event.target == lunarnautsModal ||
     event.target == timelesstaleModal ||
-    event.target == growIncModal
+    event.target == growIncModal ||
+    event.target == KindredModal ||
+    event.target == JotunModal
   ) {
     afterlifeModal.style.display = 'none';
     lunarnautsModal.style.display = 'none';
     timelesstaleModal.style.display = 'none';
     growIncModal.style.display = 'none';
+    KindredModal.style.display = 'none';
+    JotunModal.style.display = 'none';
 
     iframes.forEach((iframe) => {
       iframe.contentWindow.postMessage(
