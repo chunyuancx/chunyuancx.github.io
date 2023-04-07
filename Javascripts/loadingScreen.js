0f9df390761e345dcc83d23ceb92db23f214cad3
chunyuancx <64946231+chunyuancx@users.noreply.github.com>
2023-04-07 16:31:48 +0800

    fixed loadingScreen scrollbar

--- Javascripts/loadingScreen.js
+++ Javascripts/loadingScreen.js
@@ -1,10 +1,6 @@
 const html = document.querySelector('html');
 const loadingScreen = document.querySelector('#loading-screen');
 
-loadingScreen.addEventListener('animationstart', function () {
-  html.style.overflowY = 'hidden';
-});
-
 loadingScreen.addEventListener('animationend', function () {
   setTimeout(function () {
     html.style.overflowY = 'visible';
